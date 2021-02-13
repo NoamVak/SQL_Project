cbd74385bd759d6ff3e41e50409d6570f4413cf9
NoamVak <noamvak765@gmail.com>
2021-02-13 20:35:33 +0200

    need to continue

--- app/src/main/java/com/example/sql_project/SortStudents.java
+++ app/src/main/java/com/example/sql_project/SortStudents.java
@@ -24,24 +24,25 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
     HelperDB hlp;
     Cursor crsr;
 
-    Spinner filter1,filter2;
+    Spinner filter1, filter2;
     TextView viewInfo;
-    ArrayAdapter<String> adp1,adp2;
-    ArrayList<String> names= new ArrayList<>();
-    ArrayList<String> classes= new ArrayList<>();
-    ArrayList<String> fillIn=new ArrayList<>();
-    String[] arrFilter={"filter","All the grades of one student","grades in one subject","all students grades in one subject /up"};
+    ArrayAdapter<String> adp1, adp2;
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> classes = new ArrayList<>();
+    ArrayList<String> fillIn = new ArrayList<>();
+    String[] arrFilter = {"filter", "All the grades of one student", "grades in one subject", "all students grades in one subject /up"};
     String nameLoc;
     int row;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sort_students);
 
-        filter1=(Spinner)findViewById(R.id.filter1);
-        filter2=(Spinner)findViewById(R.id.filter2);
-        viewInfo=(TextView)findViewById(R.id.viewInfo);
+        filter1 = (Spinner) findViewById(R.id.filter1);
+        filter2 = (Spinner) findViewById(R.id.filter2);
+        viewInfo = (TextView) findViewById(R.id.viewInfo);
 
         hlp = new HelperDB(this);
         db = hlp.getReadableDatabase();
@@ -50,7 +51,7 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
         filter2.setOnItemSelectedListener(this);
 
         adp1 = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item,arrFilter);
+                R.layout.support_simple_spinner_dropdown_item, arrFilter);
         filter1.setAdapter(adp1);
 
         filter2.setAdapter(null);
@@ -60,9 +61,9 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
         crsr = db.query(TABLE_STUDENTS, columns, null, null, null, null, null);
         int col1 = crsr.getColumnIndex(Students.NAME);
         crsr.moveToFirst();
-        while (!crsr.isAfterLast()){
+        while (!crsr.isAfterLast()) {
             String name = crsr.getString(col1);
-            String tmp =name;
+            String tmp = name;
             names.add(tmp);
             crsr.moveToNext();
         }
@@ -75,10 +76,10 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
         crsr = db.query(TABLE_GRADES, columns1, null, null, null, null, null);
         col1 = crsr.getColumnIndex(Grades.CLASS);
         crsr.moveToFirst();
-        while (!crsr.isAfterLast()){
+        while (!crsr.isAfterLast()) {
             String subj = crsr.getString(col1);
-            String tmp =subj;
-            if(!classes.contains(tmp))
+            String tmp = subj;
+            if (!classes.contains(tmp))
                 classes.add(tmp);
             crsr.moveToNext();
         }
@@ -86,30 +87,30 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
         db.close();
     }
 
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
 
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        String st=item.getTitle().toString();
-        if(st.equals("Grades Insertion")) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String st = item.getTitle().toString();
+        if (st.equals("Grades Insertion")) {
             Intent in = new Intent(this, GradesSubmission.class);
             startActivity(in);
         }
-        if(st.equals("View Students")) {
+        if (st.equals("View Students")) {
             Intent in = new Intent(this, ViewStudents.class);
             startActivity(in);
         }
-        if(st.equals("Credits")) {
+        if (st.equals("Credits")) {
             Intent in = new Intent(this, Credits.class);
             startActivity(in);
         }
-        if(st.equals("Sort Students")) {
+        if (st.equals("Sort Students")) {
             return false;
         }
-        if(st.equals("Student Insertion")) {
+        if (st.equals("Student Insertion")) {
             Intent in = new Intent(this, MainActivity.class);
             startActivity(in);
         }
@@ -118,49 +119,87 @@ public class SortStudents extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        Spinner s1=(Spinner)parent;
-        Spinner s2=(Spinner)parent;
+        Spinner s1 = (Spinner) parent;
+        Spinner s2 = (Spinner) parent;
         db = hlp.getReadableDatabase();
-        if(s1.getId()==filter1.getId()){
-            row=pos;
-            if(pos==0)
+        if (s1.getId() == filter1.getId()) {
+            row = pos;
+            if (pos == 0)
                 filter2.setAdapter(null);
-            else if(pos==1){
+            else if (pos == 1) {
                 adp2 = new ArrayAdapter<String>(this,
-                        R.layout.support_simple_spinner_dropdown_item,names);
+                        R.layout.support_simple_spinner_dropdown_item, names);
                 filter2.setAdapter(adp2);
-            }
-            else if(pos==2 || pos==3){
+                viewInfo.setText("");
+            } else if (pos == 2 || pos == 3) {
                 adp2 = new ArrayAdapter<String>(this,
-                        R.layout.support_simple_spinner_dropdown_item,classes);
+                        R.layout.support_simple_spinner_dropdown_item, classes);
                 filter2.setAdapter(adp2);
+                viewInfo.setText("");
             }
-        }
-        else if(s2.getId()==filter2.getId()){
-            if(row==1){
-                nameLoc= names.get(pos);
-                String[] columns = {Grades.CLASS,Grades.GRADE};
-                String selection=  Grades.KEY_ID+"=?";
-                String [] selectionArgs= {String.valueOf(pos)};
+        } else if (s2.getId() == filter2.getId()) {
+            if (row == 1) {
+                nameLoc = names.get(pos);
+                String[] columns = {Grades.CLASS, Grades.GRADE};
+                String selection = Grades.KEY_ID + "=?";
+                String[] selectionArgs = {String.valueOf(pos)};
 
                 crsr = db.query(TABLE_GRADES, columns, selection, selectionArgs, null, null, null);
                 int col1 = crsr.getColumnIndex(Grades.CLASS);
-                int col2= crsr.getColumnIndex(Grades.GRADE);
+                int col2 = crsr.getColumnIndex(Grades.GRADE);
                 crsr.moveToFirst();
-                while (!crsr.isAfterLast()){
+                while (!crsr.isAfterLast()) {
                     String subj = crsr.getString(col1);
-                    int grade= crsr.getInt(col2);
-                    viewInfo.append(subj+" - "+grade+"\n");
+                    int grade = crsr.getInt(col2);
+                    viewInfo.append(subj + " - " + grade + "\n");
                     crsr.moveToNext();
                 }
                 crsr.close();
                 db.close();
-            }
-            else if(row==2){
-                
+
+            } else if (row == 2) {
+                String[] columns = {Grades.KEY_ID, Grades.CLASS, Grades.GRADE};
+                String selection= Grades.CLASS+"=?";
+                String[] selectionArgs= {classes.get(pos)};
+                String orderBy = Grades.KEY_ID;
+
+                crsr = db.query(TABLE_GRADES, columns, selection, selectionArgs, null, null, orderBy);
+                int col1 = crsr.getColumnIndex(Grades.CLASS);
+                int col2 = crsr.getColumnIndex(Grades.GRADE);
+                int col3 = crsr.getColumnIndex(Grades.KEY_ID);
+                crsr.moveToFirst();
+                while (!crsr.isAfterLast()) {
+                    String subj = crsr.getString(col1);
+                    int grade = crsr.getInt(col2);
+                    nameLoc = names.get(crsr.getInt(col3));
+                    viewInfo.append(nameLoc + " - " + subj + " - " + grade + "\n");
+                    crsr.moveToNext();
+                }
+                crsr.close();
+                db.close();
+
+            } else if (row == 3) {
+                String[] columns = {Grades.KEY_ID, Grades.CLASS, Grades.GRADE};
+                String selection= Grades.CLASS+"=?";
+                String[] selectionArgs= {classes.get(pos)};
+                String orderBy = Grades.GRADE;
+
+                crsr = db.query(TABLE_GRADES, columns, selection, selectionArgs, null, null, orderBy);
+                int col1 = crsr.getColumnIndex(Grades.CLASS);
+                int col2 = crsr.getColumnIndex(Grades.GRADE);
+                int col3 = crsr.getColumnIndex(Grades.KEY_ID);
+                crsr.moveToFirst();
+                while (!crsr.isAfterLast()) {
+                    String subj = crsr.getString(col1);
+                    int grade = crsr.getInt(col2);
+                    nameLoc = names.get(crsr.getInt(col3));
+                    viewInfo.append(nameLoc + " - " + subj + " - " + grade + "\n");
+                    crsr.moveToNext();
+                }
+                crsr.close();
+                db.close();
 
             }
-
         }
     }
 
